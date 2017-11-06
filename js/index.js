e34bc59d20db5db56120e2748d7d6671f6d1d75a
guyunxiang <guyunxiang32@gmail.com>
2017-11-06 16:52:38 +0800

    deploy master

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(function(){
 
   var url = window.location.href;
+  var searchReg = null;
 
   // 监听搜索框回车事件
   $('.search > input').on('keypress', function(e) {
@@ -28,17 +29,19 @@ $(function(){
     var resultList = [];
     var el = document.createElement('div');
     if (!searchVal) { window.location.href = '/'; }
+    searchReg = new RegExp(searchVal, 'gi');
     $.get('../content.json', function(data) {
       data.forEach(function(item) {
         el.innerHTML = item.text;
         if (
-          item.title.indexOf(searchVal) > -1 ||
-          el.innerHTML.indexOf(searchVal) > -1
+          item.title.search(searchReg) > -1 ||
+          el.innerHTML.search(searchReg) > -1 ||
+          (item.keywords || []).join().search(searchReg) > -1
         ) {
           resultList.push(item);
         }
       });
-      renderResultList(resultList);
+      renderResultList(resultList, searchVal);
     });
   }
 
@@ -51,21 +54,25 @@ $(function(){
   }
 
   // 渲染结果列表
-  function renderResultList(dataList) {
+  function renderResultList(dataList, searchVal) {
     var scEl = $('.search-content');
     if (dataList.length) {
       var templateHtml = '<ul class="listing">';
       dataList.forEach(function(item) {
+        var itemTitle = item.title.replace(searchReg, '<span class="search-hot-words">$&</span>');
         templateHtml +=
           '<div class="listing-item">' +
             '<div class="list-post">' +
               '<a href="/'+ item.path +'" title="'+ item.title +'">' +
-                item.title +
+                itemTitle +
               '</a>' +
               '<div class="post-time">' +
                 '<span class="date">' +
                   item.date +
                 '</span>' +
+              '</div>' +
+              '<div class="post-detail">' +
+                getContext(item) +
               '</div>' +
             '</div>' +
           '</div>';
@@ -75,8 +82,31 @@ $(function(){
     } else {
       scEl.find('.loading-result').hide();
       scEl.find('.search-not-found').show();
+      scEl.find('.search-not-found > .search > input').focus();
+      $('.page-top > .information > .search').hide();
     }
     $('.post-information > .meta > .info').html('本次共搜索到 ' + dataList.length + ' 条结果');
+  }
+
+  // 截取搜索匹配的大致文章内容
+  function getContext(item) {
+    var el = document.createElement('div');
+    el.innerHTML = item.text;
+    var index = el.innerHTML.search(searchReg);
+    var preText = '';
+    var nextText = '';
+    // 通过关键字匹配进来，模糊匹配上的，这种情况直接从开头取
+    if (index < 0) {
+      nextText = el.innerHTML.substr(0, 150).trim();
+    } else {
+      preText = el.innerHTML.substring(el.innerHTML.substring(0, index).lastIndexOf('。') + 1, index).replace(/(^\s*)/g,"");
+      // preText 最大不超过 80 个字符串
+      if (preText.length > 80) {
+        preText = el.innerHTML.substring(index - 80, index).trim();
+      }
+      nextText = el.innerHTML.substr(index, (150 - preText.length)).trim();
+    }
+    return (preText + nextText).replace(searchReg, '<span class="search-hot-words">$&</span>') + '...';
   }
 
 });
